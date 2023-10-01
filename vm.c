199a38f5fff95fb148b2338a39ceb8a4ffd99e09
luc1236060 <lau2000g@gmail.com>
2023-10-01 18:18:17 -0400

    vm.c

--- vm.c
+++ vm.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "regname.h"
 #include "machine_types.h"
 #include "instruction.h"
 // a size for the memory (2^16 bytes = 64K)
@@ -10,3 +15,42 @@ static union mem_u
      word_type words[MEMORY_SIZE_IN_WORDS];
      bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
 } memory;
+
+
+int main(int argc, char * argv[]){
+
+     int PC;
+     word_type reg[NUM_REGISTERS];
+
+     if(strcmp(argv[1], "-p")){
+
+          BOFFILE bf = bof_read_open(argv[2]);
+          
+          BOFHeader bh = bof_read_header(bf);
+          
+          for(PC = 0; PC < bh.text_length/BYTES_PER_WORD; PC++){
+
+               bin_instr_t bi = instruction_read(bf);     
+               memory.instrs[PC] = bi;
+
+          }
+
+          for(int i = 0; i < bh.data_length/BYTES_PER_WORD;){
+
+               memory.words[bh.data_start_address + i] = bof_read_word(bf);
+
+          }
+          
+          for(int i = 0; i < bh.data_length/BYTES_PER_WORD; i++){
+               instruction_print( , , memory.instrs[i]);
+          }
+
+
+     } else {
+
+      
+
+     }
+
+    
+}
