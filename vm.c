82f3dc396c6413b4dfbb1117d900b4afdb49fd94
Justin Rimmeli <justinskully5228@gmail.com>
2023-10-01 14:53:18 -0400

    vm.c

---
+++ vm.c
@@ -0,0 +1,12 @@
+#include "machine_types.h"
+#include "instruction.h"
+// a size for the memory (2^16 bytes = 64K)
+#define MEMORY_SIZE_IN_BYTES (65536 - BYTES_PER_WORD)
+#define MEMORY_SIZE_IN_WORDS (MEMORY_SIZE_IN_BYTES / BYTES_PER_WORD)
+
+static union mem_u
+{
+     byte_type bytes[MEMORY_SIZE_IN_BYTES];
+     word_type words[MEMORY_SIZE_IN_WORDS];
+     bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
+} memory;
