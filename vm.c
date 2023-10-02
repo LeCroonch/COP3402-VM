072de876de48f643c02179d3ebac8bfdb3a4164a
luc1236060 <lau2000g@gmail.com>
2023-10-01 22:06:17 -0400

    Update vm.c

--- vm.c
+++ vm.c
@@ -22,41 +22,59 @@ static union mem_u
 int main(int argc, char * argv[]){
 
      int PC;
+     
      word_type reg[NUM_REGISTERS];
+ 
+     if(strcmp(argv[1], "-p") == 0){
 
-     if(strcmp(argv[1], "-p")){
-
-          BOFFILE bf = bof_read_open(argv[2]);
+          char* fileName = argv[2];
+ 
+          BOFFILE bf = bof_read_open(fileName);
           
           BOFHeader bh = bof_read_header(bf);
-          
+          printf("%d\n", bh.data_start_address);
+          printf("%-5s: %s\n", "Addr", "Instruction");
           for(int i = 0; i < bh.text_length/BYTES_PER_WORD; i++){
-
+      
                bin_instr_t bi = instruction_read(bf);     
                memory.instrs[i] = bi;
+               printf("   %d %s\n", PC, instruction_assembly_form(memory.instrs[i]));
+               PC = PC + 4;
+          }
+          
+          if((bh.data_length/BYTES_PER_WORD) == 0){
+               printf("%-5u: %d\n", bh.data_start_address, memory.words[0]);   
+          }
+          
+          
 
+          for(int i = 0; i < bh.data_length/BYTES_PER_WORD; i++){
+
+               printf("%-5u: %d\n", bh.data_start_address, memory.words[i]);
+               bh.data_start_address = bh.data_start_address + 4;
           }
 
-          for(int i = 0; i < bh.data_length/BYTES_PER_WORD;){
+          
 
+         
+     } else {
+ 
+          BOFFILE bf = bof_read_open(argv[1]);
+
+          BOFHeader bh = bof_read_header(bf);
+
+       
+          for(int i = 0; i < bh.data_length/BYTES_PER_WORD; i++){
+     
                memory.words[bh.data_start_address + i] = bof_read_word(bf);
 
           }
-          
+               
           for(int i = 0; i < bh.data_length/BYTES_PER_WORD; i++){
 
                printf("a%d;\t%s", PC, instruction_assembly_form(memory.instrs[i]));
                PC = PC + 4;
           }
-
-          exit(0);
-     } else {
-
-          BOFFILE bf = bof_read_open(argv[1]);
-
-          BOFHeader bh = bof_read_header(bf);
-
-          
      }
 
     
