953ca655b070459dd7b7ebb40056e533867347df
luc1236060 <lau2000g@gmail.com>
2023-10-01 23:43:04 -0400

    vm.c

--- vm.c
+++ vm.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "utilities.h"
 #include "bof.h"
-#include "instruction.h"
 #include "regname.h"
 #include "machine_types.h"
 #include "instruction.h"
@@ -21,7 +21,7 @@ static union mem_u
 
 int main(int argc, char * argv[]){
 
-     int PC;
+     int PC = 0;
      
      word_type reg[NUM_REGISTERS];
  
@@ -32,26 +32,34 @@ int main(int argc, char * argv[]){
           BOFFILE bf = bof_read_open(fileName);
           
           BOFHeader bh = bof_read_header(bf);
-          printf("%d\n", bh.data_start_address);
+          
           printf("%-5s: %s\n", "Addr", "Instruction");
           for(int i = 0; i < bh.text_length/BYTES_PER_WORD; i++){
       
                bin_instr_t bi = instruction_read(bf);     
                memory.instrs[i] = bi;
+               
                printf("   %d %s\n", PC, instruction_assembly_form(memory.instrs[i]));
                PC = PC + 4;
           }
           
-          if((bh.data_length/BYTES_PER_WORD) == 0){
-               printf("%-5u: %d\n", bh.data_start_address, memory.words[0]);   
-          }
-          
-          
+  
 
-          for(int i = 0; i < bh.data_length/BYTES_PER_WORD; i++){
+     
+          
+          int currAddr = bh.data_start_address;
+          
+          for(int i = bh.data_start_address; i < bh.stack_bottom_addr; i+4){
 
-               printf("%-5u: %d\n", bh.data_start_address, memory.words[i]);
-               bh.data_start_address = bh.data_start_address + 4;
+               printf("%-5u: %d", currAddr, memory.words[currAddr/BYTES_PER_WORD]);
+
+               if(memory.bytes[i] == 0){
+                    printf(" ...\n");
+                    break;
+
+               }
+               
+               currAddr = currAddr + 4;               
           }
 
           
@@ -66,22 +74,67 @@ int main(int argc, char * argv[]){
        
           for(int i = 0; i < bh.data_length/BYTES_PER_WORD; i++){
      
-               memory.words[bh.data_start_address + i] = bof_read_word(bf);
+               bin_instr_t bi = instruction_read(bf);
+               memory.instrs[i] = bi;
+               instructionCycle(memory.instrs[i]);
 
           }
                
-          for(int i = 0; i < bh.data_length/BYTES_PER_WORD; i++){
-
-               printf("a%d;\t%s", PC, instruction_assembly_form(memory.instrs[i]));
-               PC = PC + 4;
-          }
      }
 
     
 }
 
-void instruction(int PC){
+const char *instructionCycle(bin_instr_t instr){
+     char *buf;
+     char * name;
+     instr_type it = instruction_type(instr);
+     printf("%d\n", it);
+     switch(it){
+          case syscall_instr_type:
+          break;
+          case reg_instr_type:
+          switch (instr.reg.func){
+               
+               case ADD_F:
+               break;
+               case SUB_F:
+               break;
+               case MUL_F:
+               break;
+               case DIV_F:
+               break;
+               case MFHI_F:
+               break;
+               case MFLO_F:
+               break;
+               case AND_F:
+               break;
+               case BOR_F:
+               break;
+               case NOR_F:
+               break;
+               case XOR_F:
+               break;
+               case SLL_F:
+               break;
+               case SRL_F:
+               break;
+               case JR_F:
+               break;
+               case SYSCALL_F:
+               break;
 
+          }
+          case immed_instr_type:
+          break;
+          case jump_instr_type:
+          break;
+          default:
+          bail_with_error("Uknown instruction type (%d) in instruction_assembly_form!", it);
 
-     PC = PC + 4;
+          return buf;
+     }
+
+   
 }
